9b2ee794cfccfe260bfecce6dfe27a59fb69a37d
KDB6 <ipkg7210@gmail.com>
2022-10-11 22:51:56 +0900

    10 11
    10 11

--- project/include/header.php
+++ project/include/header.php
@@ -9,9 +9,9 @@
     <link rel="stylesheet" href="../asset/css/reset.css">
     <link rel="stylesheet" href="../asset/css/common.css">
     <link rel="stylesheet" href="../asset/css/header.css">
+    
     <style>
                
-        
     </style>
 </head>
 
